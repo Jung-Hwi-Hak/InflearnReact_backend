d0175b0124a2b678c9d3d53a66afb066d94a9276
정휘학 <gnlgrkdl@danusys.com>
2023-07-03 04:56:46 +0900

    게시글 생성, 조회 api

--- class_backend/index.ts
+++ class_backend/index.ts
@@ -9,23 +9,85 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 
 // API DOCS
 const typeDefs = `#graphql
+  input CreateBoardInput{
+    writer: String
+    title: String
+    contents: String
+  }
+
+  type MyBoard {
+    number: Int
+    title: String
+    writer: String
+    contents: String
+  }
+
   type Query {
-    hello: String
-    test: String
+    fetchBoard: [MyBoard]!
+  }
+
+  type Mutation {
+    # 연습용
+    #createBoard(writer: String, title: String, contents: String): String
+
+    # 실무용
+    createBoard(createBoardInput:CreateBoardInput):String
   }
 `;
 
 // API
 const resolvers = {
   Query: {
-    hello: () => "world",
-    test: () => "test",
+    fetchBoard: async () => {
+      // 모두 조회
+      const result = await Board.find();
+      console.log(result);
+
+      // 단건 조회
+      // const result = await Board.findOne({
+      //   where: { number: 3 },
+      // });
+      // console.log(result);
+
+      return result;
+    },
+  },
+  Mutation: {
+    createBoard: async (parent: any, args: any, context: any, info: any) => {
+      await Board.insert({
+        ...args.createBoardInput,
+        // title: args.createBoardInput.title,
+        // writer: args.createBoardInput.writer,
+        // contents: args.createBoardInput.contents,
+      });
+      return "게시글 등록에 성공!";
+    },
+
+    // updateBoard: async () => {
+    // Board.update({조건},{수정내용})
+    // await Board.update({ number: 3 }, { writer: "영희" });
+    // },
+
+    // deleteBoard: async () => {
+    //   // 진짜 삭제
+    //   // await Board.delete({ number: 3 });
+    //   // 삭제로 취급 ( 복원 가능 ) - 소프트삭제
+    //   // await Board.update({number: 3}, {isDeleted: true})
+    //   // 삭제로 취급 - deletedAt 값이 NULL 이면 삭제 X
+    //   // await Board.update({ number: 3 }, { deletedAt: new Date() });
+    // },
   },
 };
 
+// @ts-ignore
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // cors: true,
+  // 선택적 풀기
+  // cors: {
+  //   origin: ["http://naver.com", "http://coupang.com"]
+  // }
 });
 
 const AppDataSource = new DataSource({
